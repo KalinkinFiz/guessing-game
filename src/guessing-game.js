83d59e1c33e26b4b5329c6c2229d356f6e64ae2f
Maxim Kalinin <kalinkin.fiz@gmail.com>
2021-07-15 02:36:25 +0300

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    return this.min + Math.ceil((this.max - this.min) / 2);
+  }
 
-    guess() {
+  lower() {
+    this.max = this.guess();
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guess();
+  }
 }
 
 module.exports = GuessingGame;
